2e9ab816e0d689112b7d9c1ed558e04ae92898eb
mgendi <mgendiisaac@live.com>
2015-06-19 12:27:37 +1200

    added website

---
+++ js/data.js
@@ -0,0 +1,7 @@
+var codropsEvents = {
+	'06-06-2015' : '<a href="#">Sabbath <br>Kids Church</a>',
+	'06-13-2015' : '<a href="#">Sabbath <br>Kids Church</a>',
+	'06-20-2015' : '<a href="#">Sabbath <br>Kids Church</a>',
+	'06-27-2015' : '<a href="#">Sabbath <br>Kids Church</a>',
+	};
+	
